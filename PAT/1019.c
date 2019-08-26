5925a580e152588657858d2720fa3b71122333a0
xuelian6 <511167495@qq.com>
2019-08-26 18:27:08 +0800

    new

--- PAT/1019.c
+++ PAT/1019.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int upper(int n){
-	int i, j, temp, a[4];
+	int i, j, temp, a[4], x;
 	
 	for(i = 0; i < 4; i++){
 		a[i] = n % 10;
@@ -9,21 +9,22 @@ int upper(int n){
 	}
 	for(i = 0; i < 3; i++){
 		for(j = 0; j < 3 - i; j++){
-			if(a[i] > a[i + 1]){
-				temp = a[i + 1];
-				a[i + 1] = a[i];
-				a[i] = temp;
+			if(a[j] > a[j + 1]){
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
 		}
 	}
+	x = a[0];
 	for(i = 0; i < 3; i++){
-		x = a[i] * 10 + a[i + 1];
+		x = x * 10 + a[i + 1];
 	}
 	return x;
 }
 
 int lower(int n){
-	int i, j, temp, a[4];
+	int i, j, temp, a[4], x;
 	
 	for(i = 0; i < 4; i++){
 		a[i] = n % 10;
@@ -31,15 +32,16 @@ int lower(int n){
 	}
 	for(i = 0; i < 3; i++){
 		for(j = 0; j < 3 - i; j++){
-			if(a[i] < a[i + 1]){
-				temp = a[i + 1];
-				a[i + 1] = a[i];
-				a[i] = temp;
+			if(a[j] < a[j + 1]){
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
 		}
 	}
+	x = a[0];
 	for(i = 0; i < 3; i++){
-		x = a[i] * 10 + a[i + 1];
+		x = x * 10 + a[i + 1];
 	}
 	return x;
 }
@@ -48,18 +50,26 @@ int main(void){
 	
 	int n, diff, up, low;
 	
-	scanf("%d", n);
+	scanf("%d", &n);
 	
 	up = upper(n);
 	low = lower(n);
 	diff = low - up;
 	
-	while(diff != 6174){
-		printf("%d - %d = %d", low, up, diff);
-		up = upper(diff);
-		low = lower(diff);
-		diff = low - up;
+	if(up != low){
+		do{
+			printf("%04d - %04d = %d\n", low, up, diff);
+			up = upper(diff);
+			low = lower(diff);
+			diff = low - up;
+		}while(diff != 6174);
+		
+		printf("%04d - %04d = %d\n", low, up, diff);
 	}
+	else{
+		printf("%04d - %04d = %04d\n", low, up, diff);
+	}
+	
 	
 	return 0;
 }
